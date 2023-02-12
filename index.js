461e0014500b9785d5c974694f8492b31f9be1ae
ifeanyi_ns <ifeanyi_ns@icloud.com>
2023-02-12 10:33:26 +0100

    Last Commit

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const sc = document.getElementById('sc')
 const deg = 6;
 
 setInterval(()=>{
-    let day = new Date()
-    let ms = day.getMilliseconds() * deg
-    let hh = day.getHours * 30
+    let day = new Date();
+    let ms = day.getMilliseconds() * deg;
+    let hh = day.getHours * 30;
     let mm = day.getMinutes() * deg
-    let ss = day.getSeconds() * deg + ms / 9000
+    let ss = day.getSeconds() * deg + ms / 9000;
 
     hr.style.transform = `rotateZ(${hh}deg)`
     mn.style.transform = `rotateZ(${mm}deg)`
